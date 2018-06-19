a62156a4898f91034f713fab13ac35a6a6b1f1e7
joao.carmo <joao.carmo@securibox.fr>
2018-06-19 12:56:28 +0100

    Create AdditionalAuthData and AdditionalAuthRequest models

---
+++ Securibox.CloudAgents/src/Securibox.CloudAgents/Api/Documents/models/AdditionalAuthRequest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Securibox.CloudAgents.Api.Documents.Models
+{
+    public class AdditionalAuthRequest
+    {
+        /// <summary>
+        /// Customer account identifier
+        /// </summary>
+        public string CustomerAccountId { get; set; }
+        /// <summary>
+        /// The secret code for multi factor authentication
+        /// </summary>
+        public string SbxSecretCode { get; set; }
+        /// <summary>
+        /// Initializes the AdditionalAuthRequest class.
+        /// </summary>
+        /// <param name="customerAccountId"></param>
+        /// <param name="sbxSecretCode"></param>
+        public AdditionalAuthRequest(string customerAccountId, string sbxSecretCode)
+        {
+            this.CustomerAccountId = customerAccountId;
+            this.SbxSecretCode = sbxSecretCode;
+        }
+    }
+}
