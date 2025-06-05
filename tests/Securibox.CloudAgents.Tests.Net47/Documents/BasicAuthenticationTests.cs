3503042f0f541fb52b9840d797d51249d5891837
cstruman <charles.struman@securibox.eu>
2025-06-05 16:14:07 +0100

    Correction baseAdress

--- tests/Securibox.CloudAgents.Tests.Net47/Documents/BasicAuthenticationTests.cs
+++ tests/Securibox.CloudAgents.Tests.Net47/Documents/BasicAuthenticationTests.cs
@@ -15,7 +15,7 @@ namespace Securibox.CloudAgents.Tests.Net47.Documents
         public BasicAuthenticationTests()
         {
             BasicAuthConfig basicAuthConfig = new BasicAuthConfig("[UserName]", "[Password]");
-            _apiClient = new ApiClient("http://localhost:8080/api/v1", basicAuthConfig, true);
+            _apiClient = new ApiClient("http://localhost:8080", basicAuthConfig, true);
         }
 
         [TestMethod]
